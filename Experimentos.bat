6d4d782bda0f0507db1d317a652d5f81eacb222c
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-09-10 15:35:27 -0300

    Testes e arquivos
    Testa domínios específicos e guarda os resultados em arquivos específicos

---
+++ Experimentos.bat
@@ -0,0 +1,11 @@
+echo Teste realizado as %time% > trTufpe.txt
+tracert www.ufpe.br >> trTufpe.txt
+
+echo Teste realizado as %time% > trTufrj.txt
+tracert www.ufrj.br >> trTufrj.txt
+
+echo Teste realizado as %time% > trTutok.txt
+tracert www.u-tokyo.ac.jp >> trTutok.txt
+
+echo Teste realizado as %time% > trTlip6.txt
+tracert www.lip6.fr >> trTlip6.txt
