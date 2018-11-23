3486b3ed65e997f3e0c2c9181693129dbb4b2f9d
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-11-23 00:28:36 -0300

    Deve estar funcionando

--- ServidorChat.java
+++ ServidorChat.java
@@ -1,71 +1,40 @@
-import java.util.*;
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.lang.*;
+import java.time.*;
 
-int contador1, usuarios; DataInputStream entrada;
+class ServidorChat {
+    public static void main(String[] args) throws IOException {
+        String usuario1, usuario2;
 
-public class Mensagem {
-    public String padrao = "";
-    public Socket padrao;
-
-    void receber(String padraoMensagem, Socket padraoMensagem){
-        DataInputStream DISMensagem = new DataInputStream(padraoMensagem.getIntputStream());
-    }
-
-    void enviar(String padraoMensagem, Socket padraoMensagem){
-        DataOutputStream DOSMensagem = new DataOutputStream(padraoMensagem.getOutputStream());
-    }
-}
-
-public class difusaoMensagem extends Thread {
-    private Mensagem enviarMensagem;
-    public difusaoMensagem(String paraTodos, int quantosTem){
-        // Aqui vai ser aquele DataInput blah
-    }
-    
-    public void run(enviarMensagem){
-        enviarMensagem.enviar(enviarMensagem);
-    }
-}
-
-
-public class captarMensagem extends Thread {
-    private Mensagem receberMensagem;
-    String captarMensagem(DataInputStream entrada){
-        // Aqui vai ser aquele DataInput blah
-    }
-
-    public void run(receberMensagem){
-        receberMensagem.receber(receberMensagem);
-    }
-}
-
-public class ServidorChat {
-    public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        int portas[] = new int[100]; // Porta de usuários
-        String address = "localhost";
-        ServetSocket sockets[] = new Socket[100];
-        usuarios = 0; 
+        int portaDefault = 1001;
+        String mensagem, usuario1Porta = "", usuario2Porta = "";
+        ServerSocket servidor = new ServerSocket(portaDefault);
+        
         try {
-            while (true){
-                ServerSocket tempsocket = sockets[usuarios];
-                Socket socket = tempsocket.accept();
-                if(sockets[usuarios] != socket){
-                    usuarios++; // Vai preenchendo usuários 
-                                // no array de sockets do server
-                }
+            Socket aceitarConexao = servidor.accept();
+            while(true){
+                System.out.println("Conexão iniciada com sucesso.");
+                int porcentagemEnvio = 0;
+                mensagem = "a";
+                while(!mensagem.equals("")){
+                    DataInputStream inputTexto = new DataInputStream(aceitarConexao.getInputStream());
+                    DataOutputStream outputTexto = new DataOutputStream(aceitarConexao.getOutputStream());
 
-                Mensagem cliente1 = new Mensagem();
-                Mensagem cliente2 = new Mensagem();
-                Thread a = new captarMensagem(cliente1);
-                Thread b = new captarMensagem(cliente2);
-                Thread x = new difusaoMensagem(cliente1);
-                Thread y = new difusaoMensagem(cliente2);
+                    mensagem = inputTexto.readUTF();
 
-                a.start(); b.start(); x.start(); y.start();
+                    outputTexto.writeUTF(mensagem);
+                    outputTexto.writeUTF("1010"); // Destino
+                    outputTexto.writeUTF("1011"); // Você
+
+                    if(!mensagem.equals("a")){
+                        break;
+                    }
                 }
             }
+        } catch (Exception e) {
+            System.out.println("Isso me deixou envergonhado: Erro no servidor número ");
         }
     }
 }
