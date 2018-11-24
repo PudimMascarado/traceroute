324c946490a90677ede9d35d9ba9111742b171cd
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-11-23 21:29:02 -0300

    Era para estar funcionando sem erros

--- ServidorChat.java
+++ ServidorChat.java
@@ -5,30 +5,44 @@ import java.lang.*;
 import java.time.*;
 
 class ServidorChat {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String usuario1, usuario2;
 
-        int portaDefault = 1001;
-        String mensagem, usuario1Porta = "", usuario2Porta = "";
+        int portaDefault = 1000;
+        String mensagemInterceptada, usuario1Porta, usuario2Porta;
         ServerSocket servidor = new ServerSocket(portaDefault);
         
         try {
-            Socket aceitarConexao = servidor.accept();
+            
             while(true){
+                Socket aceitarConexao = servidor.accept();
                 System.out.println("Conexão iniciada com sucesso.");
-                int porcentagemEnvio = 0;
-                mensagem = "a";
-                while(!mensagem.equals("")){
+                mensagemInterceptada = "a";
+
+                while(!mensagemInterceptada.equals("")){
                     DataInputStream inputTexto = new DataInputStream(aceitarConexao.getInputStream());
                     DataOutputStream outputTexto = new DataOutputStream(aceitarConexao.getOutputStream());
 
-                    mensagem = inputTexto.readUTF();
+                    // Cliente se identificou como cliente 1 ou 2 de maneira corretamente
 
-                    outputTexto.writeUTF(mensagem);
-                    outputTexto.writeUTF("1010"); // Destino
-                    outputTexto.writeUTF("1011"); // Você
+                    if(mensagemInterceptada.toLowerCase().equals("cliente 1")){
+                        mensagemInterceptada = "Cliente 1";
+                        usuario1Porta = "1234";
+                        usuario2Porta = "5678";
+                    } else {
+                        mensagemInterceptada = "Cliente 2";
+                        usuario1Porta = "5678";
+                        usuario2Porta = "1234";
+                    }
 
-                    if(!mensagem.equals("a")){
+                    mensagemInterceptada = inputTexto.readUTF();
+
+                    outputTexto.writeUTF(mensagemInterceptada);
+                    outputTexto.writeUTF(usuario2Porta); // Destino
+                    outputTexto.writeUTF(usuario1Porta); // Você
+
+                    if(mensagemInterceptada.toLowerCase().equals("encerrar conexão")){
+                        System.out.printf("Parece que o outro cliente quis encerrar a conexão!\n Adeus, ARPANet desligando!");
                         break;
                     }
                 }
