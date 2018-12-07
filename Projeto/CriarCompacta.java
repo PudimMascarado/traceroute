73181390eb9aff411dd6ac58396f3ab885c7a568
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-12-07 20:13:22 -0200

    Proto-Arquivo

---
+++ Projeto/CriarCompacta.java
@@ -0,0 +1,16 @@
+import java.util.zip.ZipFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class CriaCompacta {
+    boolean CriaArquivo(String nome){
+        try {
+            
+        } catch (Exception naoPudeCriar) {
+            Systm.out.println("Não consegui criar o arquivo por alguma causa");
+            return false;
+        }
+    }
+
+}
